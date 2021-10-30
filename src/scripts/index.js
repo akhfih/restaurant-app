b8cfe1526ad112d97a3c3382a92f93a601f2c919
akhmad fikron huda <akhfih94@gmail.com>
2021-10-30 22:25:33 +0700

    navbar agak fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,19 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.scss";
 
-const toggleButton = document.getElementsByClassName("navbar__toggle")[0];
+/* const toggleButton = document.getElementsByClassName("navbar__toggle")[0];
 const navbarLinks = document.getElementsByClassName("navbar__link");
 
 toggleButton.addEventListener("click", function () {
   for (var i = 0; i < navbarLinks.length; i++) {
     navbarLinks[i].classList.toggle("active");
   }
+}); */
+
+const toggle = document.querySelector(".toggle");
+const nav = document.querySelector("nav ul");
+
+toggle.addEventListener("click", function (event) {
+  nav.classList.toggle("open");
+  event.stopPropagation();
 });
