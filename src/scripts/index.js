ee987041e2a81de027b4ccb47433e1c97ea103b5
akhmad fikron huda <akhfih94@gmail.com>
2021-11-02 16:34:54 +0700

    explorer restaurant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,35 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.scss";
-
-/* const toggleButton = document.getElementsByClassName("navbar__toggle")[0];
-const navbarLinks = document.getElementsByClassName("navbar__link");
-
-toggleButton.addEventListener("click", function () {
-  for (var i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].classList.toggle("active");
-  }
-}); */
+import data from "../DATA.json";
 
 const toggle = document.querySelector(".toggle");
 const nav = document.querySelector("nav ul");
+const listRestaurant = document.querySelector(".restaurant");
 
 toggle.addEventListener("click", function (event) {
   nav.classList.toggle("open");
   event.stopPropagation();
+});
+
+console.log(listRestaurant);
+data.restaurants.forEach((element) => {
+  console.log(element.city);
+  const listItemRestaurant = ` <div class="items">
+                          <img
+                            class="items__img"
+                            src="${element.pictureId}"
+                            alt="${element.name}"
+                          />
+                          <div class="items__sub">
+                            <h3 class="items__sub__title"><a href="#">${element.name}</a></h3>
+                            <p class="city">${element.city}</p>
+                            <span class="items__sub__bintang"><img src="./logo/bintang.svg" alt="bintang">(${element.rating})</span>
+                            <p class="items__sub__text">
+                            ${element.description} ....
+                            </p>
+                          </div>
+                        </div>
+`;
+
+  listRestaurant.innerHTML += listItemRestaurant;
 });
